071d351c6b8f97e125153c2b26ac15a75a76c49c
thomashammes <thomas.hammes@hotmail.com>
2021-09-12 19:42:16 -0300

    adicionei acerto, chance de critico e novas armas

--- PrototipoRPG.cpp
+++ PrototipoRPG.cpp
@@ -14,12 +14,14 @@ struct Arma {
     int danoMinimo;
     int danoMaximo;
     Genero genero;
+    int precisao;
 };
 
 struct Inimigo {
     string nome;
     int vida;
     Arma arma;
+    int defesa = 10;
 };
 
 struct Fase {
@@ -32,6 +34,12 @@ struct Jogador {
     int vida;
     string nome;
     Arma arma;
+    int defesa = 10;
+};
+
+struct Acerto {
+    int resultado;
+    bool sucesso;
 };
 
 template<typename T>
@@ -43,81 +51,137 @@ bool morreu(T personagem) {
     }
 }
 
+/*
+template<typename T, typename T2>
+bool acerto(T atacante, T2 defensor){
+    int resultado = rand() % (20) + 1;
+    bool critico = false;
+    cout << endl << atacante.nome << " jogou " << resultado + atacante.arma.precisao << " para acertar e ";
+    if (resultado + atacante.arma.precisao >= defensor.defesa){
+        cout << "acertou.";
+        if (resultado == 20) {
+            cout << " E foi um golpe CRÍTICO!";
+        }
+        return true;
+    } else {
+        cout << "errou. \n";
+        return false;
+    }
+}
+*/
+
+template<typename T, typename T2>
+Acerto acerto(T atacante, T2 defensor){
+    Acerto acerto = {false, false};
+    acerto.resultado = rand() % (20) + 1;
+    cout << endl << atacante.nome << " jogou " << acerto.resultado + atacante.arma.precisao << " para acertar e ";
+    if (acerto.resultado + atacante.arma.precisao >= defensor.defesa){
+        cout << "acertou";
+        acerto.sucesso = true;
+        if (acerto.resultado == 20) {
+            cout << " um golpe CRÍTICO";
+        }
+        cout << "!";
+    } else {
+        cout << "errou... \n";
+    }
+    return acerto;
+}
+
 template<typename T>
 int dano(T personagem) {
     int resultado = (rand() % ((personagem.arma.danoMaximo + 1) - personagem.arma.danoMinimo)) + personagem.arma.danoMinimo;
-    cout << endl << personagem.nome << " deu " << resultado << " de dano com ";
-    if (personagem.arma.genero == F){
-        cout << "sua ";
-    } else {
-        cout << "seu ";
-    }
-    cout << personagem.arma.nome << ". ";
     return resultado;
 }
 
 template<typename T, typename T2>
-T2 ataque(T personagem1, T2 personagem2) {
-    personagem2.vida -= dano(personagem1);
-    morreu(personagem2);
-    if (morreu(personagem2)) {
-        cout << "\nO " << personagem2.nome << " MORREU!\n";
-    } else {
-        cout << "\nO " << personagem2.nome << " ficou com " << personagem2.vida << " pontos de vida continua vivo.\n";
+T2 ataque(T atacante, T2 defensor) {
+    Acerto acerto1 = acerto(atacante, defensor);
+    int danoTotal;
+    if (acerto1.sucesso){
+        danoTotal = dano(atacante);
+        if (acerto1.resultado == 20){
+            danoTotal += dano(atacante);
+        }
+        defensor.vida -= danoTotal;
+        cout << endl << atacante.nome << " deu " << danoTotal << " de dano com ";
+        if (atacante.arma.genero == F){
+            cout << "sua ";
+        } else {
+            cout << "seu ";
+        }
+        cout << atacante.arma.nome << ". ";
+        if (morreu(defensor)) {
+            cout << "\nO " << defensor.nome << " ficou com " << defensor.vida << " pontos de vida e MORREU!\n";
+        } else {
+            cout << "\nO " << defensor.nome << " ficou com " << defensor.vida << " pontos de vida continua vivo.\n";
+        }
     }
-    return personagem2;
+    return defensor;
 }
 
 void jogarFase(Jogador protagonista, Fase fase) {
-  int quantidadeInimigos = sizeof(fase.inimigos)/sizeof(fase.inimigos[0]);
-  for(int i = 0; i < quantidadeInimigos; i++){
-      Inimigo inimigo = fase.inimigos[i];
-      cout << inimigo.nome << endl;
-      while (inimigo.vida > 0) {
-          inimigo = ataque<Jogador, Inimigo>(protagonista, inimigo);
-
-          protagonista = ataque<Inimigo, Jogador>(inimigo, protagonista);
-          if(protagonista.vida <= 0 ){
-              return;
-          }
-      }
-  }
+    cout << "\nEntrando em " << fase.nome << "...\n";
+    int quantidadeInimigos = sizeof(fase.inimigos)/sizeof(fase.inimigos[0]);
+    for(int i = 0; i < quantidadeInimigos; i++){
+        Inimigo inimigo = fase.inimigos[i];
+        cout << "\nUm inimigo se aproxima, e seu nome é " << inimigo.nome << ".\n";
+        while (inimigo.vida > 0) {
+            inimigo = ataque<Jogador, Inimigo>(protagonista, inimigo);
+            if (!morreu(inimigo)){
+            protagonista = ataque<Inimigo, Jogador>(inimigo, protagonista);
+            }
+            if(morreu(protagonista)){
+                cout << "\nFIM DE JOGO";
+                return;
+            }
+        }
+    }
 };
 
+Arma selecionarArma() {
+    Arma armaSelecionada;
+    int selecionar;
+    cout << "\nEscolha a sua arma: \tDano\tPrecisão \n[1] Machado\t\t\t\t12\t\t-2 \n[2] Adaga\t\t\t\t4\t\t+4 \n[3] Espada\t\t\t\t9\t\t+0 \n[4] Lança\t\t\t\t10\t\t-1 \n[5] Arco\t\t\t\t8\t\t+2\n";
+    cin >> selecionar;
+    switch (selecionar){
+        case 1:
+            armaSelecionada = machado;
+            break;
+
+    }
+    return armaSelecionada;
+}
 
 int main() {
 
-    Arma machado = {"Machado", 1, 11, M}, adaga = {"Adaga", 4, 8, F};
-    Jogador protagonista = {1, 100, "player", machado};
-    Inimigo inimigo1 = {"Goberto", 10, adaga};
-    Inimigo inimigo2 = {"Juao", 10, adaga};
-    Inimigo inimigo3 = {"Roberto", 10, adaga};
-    Inimigo inimigo4 = {"Ronielson", 10, adaga};
-    Inimigo inimigo5 = {"Chefe 1", 10, adaga};
+    Arma machado = {"Machado", 1, 12, M, -2},
+        adaga = {"Adaga", 1, 4, F, 4},
+        espada = {"Espada", 1, 9, F, 0},
+        lanca = {"Lança", 1, 10, F, -1},
+        arco = {"Arco", 1, 8, M, 2};
+    Jogador protagonista = {1, 50, "Jogador", machado};
+    Inimigo inimigo1 = {"Texugo", 10, adaga, 7},
+        inimigo2 = {"Gosmanuel", 10, espada, 8},
+        inimigo3 = {"Gosmarcos", 10, arco, 9},
+        inimigo4 = {"Gosmarcelo", 10, lanca,10},
+        inimigo5 = {"Ente Ancião", 20, machado, 11};
 
-    Fase fase1;
-    fase1.nome = "Green Hill Zone";
+    Fase fase1 = {"Floresta do Desafio"};
     fase1.inimigos[0] = inimigo1;
     fase1.inimigos[1] = inimigo2;
     fase1.inimigos[2] = inimigo3;
     fase1.inimigos[3] = inimigo4;
     fase1.inimigos[4] = inimigo5;
 
-    jogarFase( protagonista, fase1);
-
     srand(time(NULL));
 
-    /*
-    inimigo1 = ataque<Jogador, Inimigo>(protagonista, inimigo1);
-    inimigo1 = ataque<Jogador, Inimigo>(protagonista, inimigo1);
-    inimigo1 = ataque<Jogador, Inimigo>(protagonista, inimigo1);
-    */
+    //cout << "Digite o nome do personagem: ";
+    //cin >> protagonista.nome;
 
-    /*
-    do{
-    cout << dano(protagonista) << endl;
-    ataque<Jogador, Inimigo>(protagonista, inimigo1);
-    } while (inimigo1.vida > 0);
-    */
+    //selecionarArma(protagonista);
 
+    jogarFase( protagonista, fase1);
+
+    return 0;
 }
